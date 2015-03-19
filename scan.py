829b868ccaf53fc8370ce75cb8ffcedb05e0d70a
Anton Blomberg <anton@deadlock.se>
2015-03-19 14:47:31 -0700

    Add tests for scan

--- scan.py
+++ scan.py
@@ -9,70 +9,79 @@ import time
 
 
 class Scanner(threading.Thread):
-    def __init__(self, db_name, output_queue):
+    def __init__(self, db_name, output_queue, db_factory=SqliteDict):
         super().__init__()
         self.setDaemon(True)
         self.output_queue = output_queue
         self.paths_to_scan = []
         self.pool = Pool(3)
-        self.db = SqliteDict('{}.sqlite'.format(db_name), autocommit=True)
+        self.db = db_factory('{}.sqlite'.format(db_name), autocommit=True)
         self.scans_in_progress = {}
         self.scans_finished = []
         self.deletes_to_check = []
 
     def add_path(self, sync_point, path):
         sentinel = object()
-        self.pool.apply_async(self.scan_path, (sentinel, sync_point, path))
+        apply_result = self.pool.apply_async(self.scan_path,
+                                             (sentinel, sync_point, path))
+        self.scans_in_progress[sentinel] = apply_result
 
     def scan_path(self, sentinel, sync_point, root):
         for path, subdirs, files in os.walk(root):
             for name in files:
-                self.paths_to_scan.append((sync_point, False, os.path.join(path, name)))
+                self.paths_to_scan.append((sync_point,
+                                           False,
+                                           os.path.join(path, name)))
             if not files:
                 self.paths_to_scan.append((sync_point, True, path))
         self.scans_finished.append((sync_point, root))
         del self.scans_in_progress[sentinel]
 
+    def step(self):
+        if self.output_queue.qsize() > 20:
+            time.sleep(0.5)
+            return
+
+        if self.paths_to_scan:
+            sync_point, is_dir, path = self.paths_to_scan.pop(
+                random.randint(0, len(self.paths_to_scan) - 1))
+            mtime = getmtime(path)
+            record = self.db.get(path, None)
+            record_is_new = False
+            if record is not None and record['mtime'] == mtime:
+                return
+            elif record is None:
+                record_is_new = True
+                record = {'sync_points': []}
+            size = getsize(path)
+
+            record['size'] = size
+            record['mtime'] = mtime
+            record['is_dir'] = is_dir
+            record['sync_points'].append(sync_point)
+
+            if record_is_new or record['size'] != size:
+                self.output_queue.put((record['sync_points'], path, is_dir,
+                                       mtime, size, False))
+            self.db[path] = record
+        elif not self.scans_in_progress and self.scans_finished:
+            self.scans_finished.clear()
+            self.deletes_to_check = self.db.keys()
+        elif self.deletes_to_check:
+            path = self.deletes_to_check.pop(
+                random.randint(0, len(self.deletes_to_check) - 1))
+            record = self.db.get(path, None)
+            if record is not None:
+                if not exists(path):
+                    self.output_queue.put((record['sync_points'],
+                                           path,
+                                           record['is_dir'],
+                                           record['mtime'],
+                                           record['size'],
+                                           True))
+        else:
+            time.sleep(1)
+
     def run(self):
         while 1:
-            if self.output_queue.qsize() > 20:
-                time.sleep(0.5)
-                continue
-
-            if self.paths_to_scan:
-
-                sync_point, is_dir, path = self.paths_to_scan.pop(random.randint(0, len(self.paths_to_scan) - 1))
-                mtime = getmtime(path)
-                record = self.db.get(path, None)
-                record_is_new = False
-                if record is not None and record['mtime'] == mtime:
-                    continue
-                elif record is None:
-                    record_is_new = True
-                    record = {'sync_points': []}
-                size = getsize(path)
-
-                record['size'] = size
-                record['mtime'] = mtime
-                record['is_dir'] = is_dir
-                record['sync_points'].append(sync_point)
-
-                if record_is_new or record['size'] != size:
-                    self.output_queue.put((record['sync_points'], path, is_dir, mtime, size, False))
-                self.db[path] = record
-            elif not self.scans_in_progress and self.scans_finished:
-                self.scans_finished.clear()
-                self.deletes_to_check = self.db.keys()
-            elif self.deletes_to_check:
-                path = self.deletes_to_check.pop(random.randint(0, len(self.deletes_to_check) - 1))
-                record = self.db.get(path, None)
-                if record is not None:
-                    if not exists(path):
-                        self.output_queue.put((record['sync_points'],
-                                               path,
-                                               record['is_dir'],
-                                               record['mtime'],
-                                               record['size'],
-                                               True))
-            else:
-                time.sleep(1)
+            self.step()
---
+++ test/test_scan.py
@@ -0,0 +1,91 @@
+# encoding: utf-8
+import os
+import queue
+from unittest import mock
+import time
+import scan
+from os.path import join as jn
+
+
+class TestScan:
+    def setup_method(self, method):
+        self.output_queue = queue.Queue()
+        self.scanner = scan.Scanner(mock.Mock(), self.output_queue,
+                                    db_factory=mock.Mock())
+
+    def teardown_method(self, method):
+        pass
+
+    def test_add_path(self):
+        self.scanner.pool = mock.Mock()
+        path = os.path.join("some", "file.txt")
+        sync_point = "sync_point"
+        self.scanner.add_path(sync_point, path)
+        assert self.scanner.pool.apply_async.called_once_with(
+            mock.ANY, sync_point, path)
+        assert len(self.scanner.scans_in_progress) == 1
+
+    def test_scan_path(self):
+        with mock.patch('os.walk') as walk_mock:
+            walk_mock.return_value = \
+                (("folder", ("subfolder", "subfolder2"), ("file1.txt",)),
+                 (jn("folder", "subfolder"), tuple(), ("subfile1.txt",)),
+                 (jn("folder", "subfolder2"), tuple(), tuple()))
+            sentinel = object()
+            root = os.path.join("some", "path")
+            sync_point = "sync_point"
+            self.scanner.scans_in_progress[sentinel] = mock.Mock()
+            self.scanner.scan_path(sentinel, sync_point, root)
+            assert self.scanner.paths_to_scan == \
+                [(sync_point, False, jn("folder", "file1.txt")),
+                 (sync_point, False, jn("folder", "subfolder", "subfile1.txt")),
+                 (sync_point, True, jn("folder", "subfolder2"))]
+
+    def test_step_sleeps_on_full_queue(self):
+        with mock.patch('time.sleep') as sleep_mock:
+            self.scanner.output_queue = mock.Mock()
+            self.scanner.output_queue.qsize.return_value = 21
+            self.scanner.step()
+            assert sleep_mock.called_once_with(0.5)
+
+    def test_step_scans_file(self):
+        self.scanner.db.get.return_value = None
+        self.scanner.db.__setitem__ = mock.Mock()
+        sync_point = "sync_point"
+        self.scanner.paths_to_scan = \
+            [(sync_point, False, jn("some", "file.txt")),
+             (sync_point, True, jn("other", "path"))]
+        with mock.patch('scan.getmtime') as getmtime_mock:
+            mtime = time.time()
+            getmtime_mock.return_value = mtime
+            with mock.patch('scan.getsize') as getsize_mock:
+                with mock.patch('random.randint') as randint_mock:
+                    getsize_mock.return_value = 100
+                    randint_mock.return_value = 0
+                    self.scanner.step()
+                    assert self.output_queue.qsize() == 1
+                    assert self.output_queue.get_nowait() == \
+                        ([sync_point], jn("some", "file.txt"), False, mtime,
+                         100, False)
+                    self.scanner.db.__setitem__.assert_called_once_with(
+                        jn("some", "file.txt"),
+                        {"sync_points": [sync_point],
+                         "size": 100,
+                         "mtime": mtime,
+                         "is_dir": False}
+                    )
+                    self.scanner.db.__setitem__.reset_mock()
+
+                    self.scanner.step()
+                    assert self.output_queue.qsize() == 1
+                    assert self.output_queue.get_nowait() == \
+                        ([sync_point], jn("other", "path"), True, mtime, 100,
+                         False)
+                    self.scanner.db.__setitem__.assert_called_once_with(
+                        jn("other", "path"),
+                        {"sync_points": [sync_point],
+                         "size": 100,
+                         "mtime": mtime,
+                         "is_dir": True}
+                    )
+
